aa6b22cf4bdb7d4aa49b864f55a1c64c37df0341
William M. Tsikata <tsikatawill@gmail.com>
2023-08-25 11:40:36 +0000

    solve 1.1

--- 01-arrays-and-hashing/01-contains-duplicates/try.js
+++ 01-arrays-and-hashing/01-contains-duplicates/try.js
@@ -1,3 +1,13 @@
 const containsDuplicates = (nums) => {
-  // code here
+  const distinctArr = new Set();
+
+  for (const num of nums) {
+    if (distinctArr.has(num)) return true;
+    distinctArr.add(num);
+  }
+
+  return false;
 };
+
+// console.log(containsDuplicates([1, 2, 3, 4])); // return false
+// console.log(containsDuplicates([1, 2, 3, 4, 2, 5])); // return true
